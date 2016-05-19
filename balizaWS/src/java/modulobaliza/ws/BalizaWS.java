dc256836247173717f8e02cb5eb7209f6627e362
pablo de la sotta <delasotta.pablo@gmail.com>
2016-05-18 23:33:37 -0300

    Método WS Modificado para la tematica

--- balizaWS/src/java/modulobaliza/ws/BalizaWS.java
+++ balizaWS/src/java/modulobaliza/ws/BalizaWS.java
@@ -4,7 +4,7 @@ import javax.jws.*;
 @WebService (serviceName="BalizaWS")
 public class BalizaWS {
     @WebMethod(operationName="holaMundo")
-    public String holaMundo()
+    public String enviarPromocion(String nombreUsuario, String textoPromocion)
     {        
         return "hola mundo";
     }    
